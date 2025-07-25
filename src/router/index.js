72df9e73dd17876cc19c44980ec26fbd04391219
nthung-2k5 <hungthitkhia@gmail.com>
2025-07-25 21:18:40 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
+    },
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
     }
 ];
 
